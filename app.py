ca16f65a869296611ae01ca75524cf91c8822e1b
biwebsolution <biwebsolution09@gmail.com>
2024-08-29 12:59:36 -0700

    message

--- app.py
+++ app.py
@@ -8,13 +8,6 @@ import cloudinary
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "https://dan-portfolio-backend.onrender.com"}})
 
-# @app.after_request
-# def after_request(response):
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-#     response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Accept, Accept-Language, Accept-Encoding')
-#     return response
-
 cloudinary.config(
     cloud_name="dosvdonf0",
     api_key = "161476777338785",
@@ -22,12 +15,12 @@ cloudinary.config(
 )
 
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///MyPortfolioDB.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///MyPortfolioDB.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'mysecretkey'
-app.config['DBHUB_API_KEY'] = 'XdPOtCf7emmaTY4shQszPYI5V9E9ID282jjBcmFWM5_49NUKLgsz4A'
-app.config['DBHUB_DB_NAME'] = 'MyPortfolioDB.db'
-# app.config['DBHUB_USERNAME'] = 'your_dbhub_username' 
+# app.config['DBHUB_API_KEY'] = 'XdPOtCf7emmaTY4shQszPYI5V9E9ID282jjBcmFWM5_49NUKLgsz4A'
+# app.config['DBHUB_DB_NAME'] = 'MyPortfolioDB.db'
+# # app.config['DBHUB_USERNAME'] = 'your_dbhub_username' 
 
 
 db = SQLAlchemy(app)
